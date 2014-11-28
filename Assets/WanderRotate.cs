416f66187f02ac05b1a7e2a3d8c4dac32533d183
Seanna Musgrave <seannamusgrave@gmail.com>
2014-11-27 18:27:49 -0800

    Added rhino, dog, bird

---
+++ Assets/WanderRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderRotate : MonoBehaviour {
+
+	Transform targetTrans = null;
+
+	// Use this for initialization
+	void Start () {
+		targetTrans = transform;
+		//targetTrans.rotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float randomX = Random.Range (0f, 1f) - 0.5f;
+		float randomY = Random.Range (0f, 1f) - 0.5f;
+		float randomZ = Random.Range (0f, 1f) - 0.5f;
+
+		targetTrans.Rotate (new Vector3 (randomX, randomY, randomZ));
+		Quaternion MoveTrans = Quaternion.Lerp (transform.rotation, targetTrans.rotation, 0.10f);
+		transform.Rotate(MoveTrans.eulerAngles);
+	}
+}
