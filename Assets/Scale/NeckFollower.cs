fbcc06c5df6c4d1ffd34621aaa0a21482d2a46e5
Seanna Musgrave <seannamusgrave@gmail.com>
2014-12-03 23:15:36 -0800

    Working on inverse kinematics

--- Assets/Scale/NeckFollower.cs
+++ Assets/Scale/NeckFollower.cs
@@ -9,6 +9,7 @@ public class NeckFollower : MonoBehaviour {
 	public Transform spine;
 	public Transform spine1;
 	public Transform spine2;
+	public Transform avatar;
 
 	// Use this for initialization
 	void Start () {
@@ -18,17 +19,18 @@ public class NeckFollower : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
+		followHeight ();
 
 		float opposite = target.position.x;
-		float adj = 8f;
+		float adj = Mathf.Abs (target.position.y - hips.position.y);
 		float tanC = opposite / adj;
 		float angle = Mathf.Atan (tanC);
-		//Debug.Log (angle);
 		float rotationZ = angle*60f / 4;
 
-		opposite = target.position.z+5;
-		adj = 8f;
+		float scale = avatar.lossyScale.x;
+		float cameraOffset = 0.2f * scale;
+		opposite = (target.position.z-cameraOffset) - hips.position.z;
+		adj = Mathf.Abs(target.position.y - hips.position.y);
 		tanC = opposite / adj;
 		angle = Mathf.Atan (tanC);
 		float rotationX = angle * 60f / 4;
@@ -37,9 +39,29 @@ public class NeckFollower : MonoBehaviour {
 		spine.transform.localEulerAngles = new Vector3 (rotationX, 0, -rotationZ);
 		spine1.transform.localEulerAngles = new Vector3 (rotationX, 0, -rotationZ);
 		spine2.transform.localEulerAngles = new Vector3 (rotationX, 0, -rotationZ);
+	}
 
-		//Quaternion newRotation = Quaternion.Euler (0, 0, 45);
+	void followHeight()
+	{
+		float scale = avatar.lossyScale.x;
+		float cameraOffset = 0.2f * scale;
+		Vector3 hipFlatPos = new Vector3 (hips.position.x, 0, hips.position.z);
+		Vector3 targetFlatPos = new Vector3 (target.position.x, 0, target.position.z - cameraOffset);
+		float adjacent = Vector3.Distance (hipFlatPos, targetFlatPos);
+		float hypo = Vector3.Distance (hips.position, spine2.position) * 2f;
 
-		//transform.rotation.Set (newRotation.x, newRotation.y, newRotation.z, newRotation.w);
+		// Pythagorean theorem.
+
+		float opposite = Mathf.Sqrt ((hypo * hypo) - (adjacent * adjacent));
+		Debug.Log ("Opposite");
+		Debug.Log (opposite);
+
+		// The opposite will become nan if the hyptoneuse is shorter than the adjacent.
+
+		if (float.IsNaN (opposite)) {
+		    opposite = 0.4f;
+		}
+
+		avatar.localPosition = new Vector3 (avatar.localPosition.x, target.localPosition.y - ((opposite/scale)) + 0.3f, avatar.localPosition.z);
 	}
 }
