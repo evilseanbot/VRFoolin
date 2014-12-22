064d75abe623769af80c8114e855bc10f651a8af
Seanna Musgrave <seannamusgrave@gmail.com>
2014-12-21 17:03:33 -0800

    Lots of changes

---
+++ Assets/Scale/SelectiveVisibility.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectiveVisibility : MonoBehaviour {
+
+	public GameObject plane;
+	public GameObject california;
+	public GameObject house;
+	public GameObject rhino;
+
+	public GameObject moonMockMeter;
+	public GameObject moonMockKilo;
+	public GameObject moon;
+
+	public GameObject sunMockMeter;
+	public GameObject sunMockKilo;
+	public GameObject sun;
+
+	public Component[] rends; 
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void scaleChange(float scale, double playerScale) {
+
+		if (scale == 0.0000001f) {			
+			//moon.renderer.enabled = true;
+			//sun.renderer.enabled = true;
+
+			//moonMockKilo.renderer.enabled = false;
+			//sunMockKilo.renderer.enabled = false;
+		}
+
+
+		if (scale == 0.0001f) {			
+			//moon.renderer.enabled = false;
+			//sun.renderer.enabled = false;
+
+			california.renderer.enabled = true;
+			//moonMockKilo.renderer.enabled = true;
+			//sunMockKilo.renderer.enabled = true;
+
+			//moonMockMeter.renderer.enabled = false;
+			//sunMockMeter.renderer.enabled = false;
+			plane.renderer.enabled = false;
+		}
+
+		if (scale == 0.1f) {
+			//moonMockKilo.renderer.enabled = false;
+			//sunMockKilo.renderer.enabled = false;
+			california.renderer.enabled = false;
+
+			setRendering(house, true);
+			setRendering(rhino, true);
+			plane.renderer.enabled = true;
+			//moonMockMeter.renderer.enabled = true;
+			//sunMockMeter.renderer.enabled = true;
+		}
+
+		if (scale == 100) {
+
+			setRendering(house, false);
+			setRendering(rhino, false);
+		}
+	}
+
+	void setRendering(GameObject model, bool shouldRender) {
+		rends = model.GetComponentsInChildren<Renderer>();
+
+		foreach( Renderer rend in rends) {
+			rend.enabled = shouldRender;
+		}
+	}
+}
--- Assets/Scale/Shrinker.cs
+++ Assets/Scale/Shrinker.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Shrinker : MonoBehaviour {
 
 	public Transform world;
+	public GameObject selectiveVisibility;
+
+	public Material nightSky;
+	public Material daySky;
 
 	// Use this for initialization
 	void Start () {
@@ -13,50 +17,13 @@ public class Shrinker : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		double scaleDim = transform.localScale.x;
-		//Vector3 currentScale = new Vector3 (scaleDim, scaleDim, scaleDim);
-		//double scaleChange = scaleDim * 0.01;
-		//float scaleChange
 
-		// Shrink world to proper size.
-		if (scaleDim < 0.001 && world.localScale.x == 100) {
-			world.localScale = new Vector3(100000, 100000, 100000);
-		}
-		if (scaleDim < 1 && world.localScale.x == 0.1f) {
-			world.localScale = new Vector3(100, 100, 100);
-		}
-		if (scaleDim < 1000 && world.localScale.x == 0.0001f) {
-			world.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-		}
-		if (scaleDim < 1000000 && world.localScale.x == 0.0000001f) {
-			world.localScale = new Vector3(0.0001f, 0.0001f, 0.0001f);
-		}
-		if (scaleDim < 1000000000 && world.localScale.x == 0.0000000001f) {
-			world.localScale = new Vector3(0.0000001f, 0.0000001f, 0.0000001f);
-		}
-
-		// Grow world to proper size.
-		if (scaleDim > 0.001 && world.localScale.x == 100000) {
-			world.localScale = new Vector3(100, 100, 100);
-		}
-		if (scaleDim > 1 && world.localScale.x == 100) {
-			world.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-		}
-		if (scaleDim > 1000 && world.localScale.x == 0.1f) {
-			world.localScale = new Vector3(0.0001f, 0.0001f, 0.0001f);
-		}
-		if (scaleDim > 1000000 && world.localScale.x == 0.0001f) {
-			world.localScale = new Vector3(0.0000001f, 0.0000001f, 0.0000001f);
-		}
-		if (scaleDim > 1000000000 && world.localScale.x == 0.0000001f) {
-			world.localScale = new Vector3(0.0000000001f, 0.0000000001f, 0.0000000001f);
-		}
+		adjustWorldScale (scaleDim);
 
 		if (Input.GetKey("down")) {
-			//if (scaleDim > 1) {
 				double scaleChange = scaleDim * 0.99;
 				float scaleChangeF = (float)scaleChange;
 				transform.localScale = new Vector3(scaleChangeF, scaleChangeF, scaleChangeF); 
-			//}
 		}
 
 		if (Input.GetKey("up")) {
@@ -66,5 +33,67 @@ public class Shrinker : MonoBehaviour {
 		}
 
 
+	}
+
+	void adjustWorldScale(double playerScale) {
+
+		bool scaleChanged = false;
+
+		// Shrink world to proper size.
+		if (playerScale < 0.001 && world.localScale.x == 100) {
+			world.localScale = new Vector3(100000, 100000, 100000);
+			scaleChanged = true;
+		}
+		if (playerScale < 1 && world.localScale.x == 0.1f) {
+			world.localScale = new Vector3(100, 100, 100);
+			scaleChanged = true;
+		}
+		if (playerScale < 1000 && world.localScale.x == 0.0001f) {
+			world.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+			scaleChanged = true;
+		}
+		if (playerScale < 1000000 && world.localScale.x == 0.0000001f) {
+			world.localScale = new Vector3(0.0001f, 0.0001f, 0.0001f);
+			scaleChanged = true;
+		}
+		if (playerScale < 1000000000 && world.localScale.x == 0.0000000001f) {
+			world.localScale = new Vector3(0.0000001f, 0.0000001f, 0.0000001f);
+			scaleChanged = true;
+		}
+		
+		// Grow world to proper size.
+		if (playerScale > 0.001 && world.localScale.x == 100000) {
+			world.localScale = new Vector3(100, 100, 100);
+			scaleChanged = true;
+		}
+		if (playerScale > 1 && world.localScale.x == 100) {
+			world.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+			scaleChanged = true;
+		}
+		if (playerScale > 1000 && world.localScale.x == 0.1f) {
+			world.localScale = new Vector3(0.0001f, 0.0001f, 0.0001f);
+			scaleChanged = true;
+		}
+		if (playerScale > 1000000 && world.localScale.x == 0.0001f) {
+			world.localScale = new Vector3(0.0000001f, 0.0000001f, 0.0000001f);
+			scaleChanged = true;
+		}
+		if (playerScale > 1000000000 && world.localScale.x == 0.0000001f) {
+			world.localScale = new Vector3(0.0000000001f, 0.0000000001f, 0.0000000001f);
+			scaleChanged = true;
+		}
+
+		// Adjust atmosphere
+		if (playerScale > 10000000) {
+			RenderSettings.skybox = nightSky;
+		}
+		
+		if (playerScale < 10000000) {
+			RenderSettings.skybox = daySky;
+		}
+
+		if (scaleChanged) {
+			selectiveVisibility.GetComponent<SelectiveVisibility>().scaleChange(world.localScale.x, playerScale);
+		}
 	}
 }
