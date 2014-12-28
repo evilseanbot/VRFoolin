a58d97abf80b02d8f2c570dadef5c21c0ad71d87
Seanna Musgrave <seannamusgrave@gmail.com>
2014-12-27 19:44:32 -0800

    Added milky way

--- Assets/Scale/Shrinker.cs
+++ Assets/Scale/Shrinker.cs
@@ -21,13 +21,13 @@ public class Shrinker : MonoBehaviour {
 		adjustWorldScale (scaleDim);
 
 		if (Input.GetKey("down")) {
-				double scaleChange = scaleDim * 0.99;
+				double scaleChange = scaleDim * (1f - (1f * Time.deltaTime));
 				float scaleChangeF = (float)scaleChange;
 				transform.localScale = new Vector3(scaleChangeF, scaleChangeF, scaleChangeF); 
 		}
 
 		if (Input.GetKey("up")) {
-				double scaleChange = scaleDim * 1.01;
+				double scaleChange = scaleDim * (1f + (1f * Time.deltaTime));
 				float scaleChangeF = (float)scaleChange;
 				transform.localScale = new Vector3(scaleChangeF, scaleChangeF, scaleChangeF); 
 		}
@@ -38,9 +38,24 @@ public class Shrinker : MonoBehaviour {
 	void adjustWorldScale(double playerScale) {
 
 		bool scaleChanged = false;
+		float newScale;
 
 		// Shrink world to proper size.
-		if (playerScale < 0.001 && world.localScale.x == 100) {
+		for (int i = -1; i < 8; i++) {
+			if (playerScale < Mathf.Pow (10, i*3) && world.localScale.x == (float)(Mathf.Pow (10, -(i+1)*3) * 100f)) {
+				newScale = Mathf.Pow (10, -((i)*3)) * 100f;
+				world.localScale = new Vector3(newScale, newScale, newScale);
+				scaleChanged = true;
+			}
+
+
+			if (playerScale > Mathf.Pow (10, i*3) && world.localScale.x == (float)(Mathf.Pow (10, -((i)*3)) * 100f)) {
+				newScale = Mathf.Pow (10, -((i+1)*3)) * 100f;
+				world.localScale = new Vector3(newScale, newScale, newScale);
+				scaleChanged = true;
+			}
+		}
+		/*if (playerScale < 0.001 && world.localScale.x == 100) {
 			world.localScale = new Vector3(100000, 100000, 100000);
 			scaleChanged = true;
 		}
@@ -62,6 +77,10 @@ public class Shrinker : MonoBehaviour {
 		}
 		if (playerScale < 1000000000000 && world.localScale.x == 0.0000000000001f) {
 			world.localScale = new Vector3(0.0000000001f, 0.0000000001f, 0.0000000001f);
+			scaleChanged = true;
+		}
+		if (playerScale < 1000000000000000 && world.localScale.x == 0.0000000000000001f) {
+			world.localScale = new Vector3(0.0000000000001f, 0.0000000000001f, 0.0000000000001f);
 			scaleChanged = true;
 		}
 		
@@ -90,6 +109,11 @@ public class Shrinker : MonoBehaviour {
 			world.localScale = new Vector3(0.0000000000001f, 0.0000000000001f, 0.0000000000001f);
 			scaleChanged = true;
 		}
+		if (playerScale > 1000000000000000 && world.localScale.x == 0.0000000000001f) {
+			world.localScale = new Vector3(0.0000000000000001f, 0.0000000000000001f, 0.0000000000000001f);
+			scaleChanged = true;
+		}
+		*/
 
 		// Adjust atmosphere
 		if (playerScale > 10000000) {
